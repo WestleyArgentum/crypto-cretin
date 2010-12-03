99b4085b4e31c16fe7e1e9e20bba7d3ccd770af9
Kurisu87 <Kurisu87@localhost>
2010-12-02 19:32:26 -0800

    gmp library/dll

---
+++ crypto-cretin/Common.hpp
@@ -0,0 +1,9 @@
+#ifndef COMMON_HPP_
+#define COMMON_HPP_
+
+#include "WindowsHeaders.hpp"
+#include "Console.hpp"
+
+#include <iostream>
+
+#endif
---
+++ crypto-cretin/main.cpp
@@ -0,0 +1,7 @@
+int CALLBACK WinMain(HINSTANCE iInstance, HINSTANCE, LPSTR iCommandLine, int iDisplayParameters)
+{
+  CreateConsole();
+  printf( "Hello, world!" );
+
+  return 0;
+}
