14500e726a5fe9473425aabc4c9f2a97aee5d450
WestleyArgentum <WestleyArgentum@gmail.com>
2010-12-03 17:51:51 -0800

    Integrated the threading stuff

--- crypto-cretin/main.cpp
+++ crypto-cretin/main.cpp
@@ -3,75 +3,7 @@
 #include "Timer.hpp"
 
 #include "encrypt.hpp"
-
-gmp_randstate_t gRandomState;
-
-mpz_class BrentFactorization(const mpz_class &N)
-{
-  if(N%2 == 0)
-    return mpz_class(2);
-
-  mpz_class y = 0,
-            ys = 0,
-            c = 0,
-            m = 0;
-  mpz_urandomm(y.get_mpz_t(), gRandomState, N.get_mpz_t());
-  mpz_urandomm(c.get_mpz_t(), gRandomState, N.get_mpz_t());
-  mpz_urandomm(m.get_mpz_t(), gRandomState, N.get_mpz_t());
-
-  mpz_class g = 1,
-            r = 1,
-            q = 1,
-            lMin = N + 1, //bad (impossible) minimum value to start off with
-            lAbs, x, k;
-
-  while(g == 1)
-  {
-    x = y;
-
-    for(mpz_class i = 0; i < r; ++i)
-    {
-      y = ((y*y)%N + c)%N;
-    }
-
-    k = 0;
-
-    while((k < r) && (g == 1))
-    {
-      ys = y;
-
-      lMin = m < r-k ? m : r-k;
-      for(mpz_class i = 0; i < lMin; ++i)
-      {
-        y = ((y*y)%N + c)%N;
-
-        mpz_abs(lAbs.get_mpz_t(), mpz_class(x-y).get_mpz_t());
-        q = (q*lAbs)%N;
-      }
-
-      mpz_gcd(g.get_mpz_t(), q.get_mpz_t(), N.get_mpz_t());
-      k = k + m;
-    }
-
-    r = r*2;
-  }
-
-  if(g==N)
-  {
-    while(true)
-    {
-      ys = ((ys*ys)%N + c)%N;
-
-      mpz_abs(lAbs.get_mpz_t(), mpz_class(x-ys).get_mpz_t());
-      mpz_gcd(g.get_mpz_t(), lAbs.get_mpz_t(), N.get_mpz_t());
-
-      if(g > 1)
-        break;
-    }
-  }
-
-  return g;
-}
+#include "BrentJob.hpp"
 
 
 enum ButtonIDs
@@ -166,6 +98,10 @@ int CALLBACK WinMain(HINSTANCE iInstance, HINSTANCE, LPSTR iCommandLine, int iDi
   std::string N = "15641574130333";
   std::string p = "3954889", 
               q = "3954997";
+
+  BrentManager = new BrentJobManager( mpz_class(N) );
+
+  BrentManager->RunBrentFactorization( 10 );
 
   //std::string N = "15641574130333";
   //std::string p = "99990001", 
