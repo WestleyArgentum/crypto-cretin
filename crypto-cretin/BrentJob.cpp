b7f241c7dde01272b780054e8c1c6e163734b620
WestleyArgentum <WestleyArgentum@gmail.com>
2010-12-03 21:56:03 -0800

    Changed the threading... it works now. We generate p and q from n.

--- crypto-cretin/BrentJob.cpp
+++ crypto-cretin/BrentJob.cpp
@@ -12,100 +12,111 @@ Mutex mutex_gRandom;
 
 BrentJobManager::~BrentJobManager ()
 {
-    Lock lock( mutex_jobs );
+  Lock lock( mutex_jobs );
 
-    for ( unsigned i = 0; i < jobs.size(); ++i )
-    {
-        jobs[i]->Kill();
-        delete jobs[i];
-    }
+  for ( unsigned i = 0; i < jobs.size(); ++i )
+  {
+    jobs[i]->Kill();
+    delete jobs[i];
+  }
 }
 
 
 void BrentJobManager::RunBrentFactorization( unsigned num_iterations /*= 4 */ )
 { 
-    //Mutex mutex_mpz_random;
+  //Mutex mutex_mpz_random;
 
-    Lock lock ( mutex_jobs );
+  Lock lock ( mutex_jobs );
 
-    for ( unsigned i = 0; i < num_iterations; ++i )
-        jobs.push_back( new BrentJob( N, jobs.size(), &ResultBrentFactorization ) );
+  for ( unsigned i = 0; i < num_iterations; ++i )
+  {
+    jobs.push_back( new BrentJob( N, result, mutex_result, jobs.size(), &ResultBrentFactorization ) );
+    jobs.back()->Wake();
+    handles.push_back( jobs.back()->GetHandle() );
+  }
+    
 
-    for ( unsigned i = 0; i < num_iterations; ++i )
-        jobs[i]->Wake();
+}
 
+
+mpz_class BrentJobManager::GetResult()
+{
+  WaitForMultipleObjects( handles.size(), &handles[0], true, INFINITE );
+
+  //Lock lock (mutex_result);  // really this is just for kicks
+  return result;
 }
 
 
 mpz_class BrentFactorization(const mpz_class &N)
 {
-    if(N%2 == 0)
-        return mpz_class(2);
+  if(N%2 == 0)
+    return mpz_class(2);
 
-    mpz_class y = 0,
-        ys = 0,
-        c = 0,
-        m = 0;
+  mpz_class y = 0,
+    ys = 0,
+    c = 0,
+    m = 0;
 
-    {
+  {
     Lock lock( mutex_gRandom );
     mpz_urandomm(y.get_mpz_t(), gRandomState, N.get_mpz_t());
     mpz_urandomm(c.get_mpz_t(), gRandomState, N.get_mpz_t());
     mpz_urandomm(m.get_mpz_t(), gRandomState, N.get_mpz_t());
-    }
+  }
 
-    mpz_class g = 1,
-        r = 1,
-        q = 1,
-        lMin = N + 1, //bad (impossible) minimum value to start off with
-        lAbs, x, k;
+  mpz_class g = 1,
+    r = 1,
+    q = 1,
+    lMin = N + 1, //bad (impossible) minimum value to start off with
+    lAbs, x, k;
 
-    while(g == 1)
+  while(g == 1)
+  {
+    x = y;
+
+    for(mpz_class i = 0; i < r; ++i)
     {
-        x = y;
-
-        for(mpz_class i = 0; i < r; ++i)
-        {
-            y = ((y*y)%N + c)%N;
-        }
-
-        k = 0;
-
-        while((k < r) && (g == 1))
-        {
-            ys = y;
-
-            lMin = m < r-k ? m : r-k;
-            for(mpz_class i = 0; i < lMin; ++i)
-            {
-                y = ((y*y)%N + c)%N;
-
-                mpz_abs(lAbs.get_mpz_t(), mpz_class(x-y).get_mpz_t());
-                q = (q*lAbs)%N;
-            }
-
-            mpz_gcd(g.get_mpz_t(), q.get_mpz_t(), N.get_mpz_t());
-            k = k + m;
-        }
-
-        r = r*2;
+      y = ((y*y)%N + c)%N;
     }
 
-    if(g==N)
+    k = 0;
+
+    while((k < r) && (g == 1))
     {
-        while(true)
-        {
-            ys = ((ys*ys)%N + c)%N;
+      ys = y;
 
-            mpz_abs(lAbs.get_mpz_t(), mpz_class(x-ys).get_mpz_t());
-            mpz_gcd(g.get_mpz_t(), lAbs.get_mpz_t(), N.get_mpz_t());
+      lMin = m < r-k ? m : r-k;
+      for(mpz_class i = 0; i < lMin; ++i)
+      {
+        y = ((y*y)%N + c)%N;
 
-            if(g > 1)
-                break;
-        }
+        mpz_abs(lAbs.get_mpz_t(), mpz_class(x-y).get_mpz_t());
+        q = (q*lAbs)%N;
+      }
+
+      mpz_gcd(g.get_mpz_t(), q.get_mpz_t(), N.get_mpz_t());
+      k = k + m;
     }
 
-    return g;
+    r = r*2;
+  }
+
+  if(g==N)
+  {
+    while(true)
+    {
+      ys = ((ys*ys)%N + c)%N;
+
+      mpz_abs(lAbs.get_mpz_t(), mpz_class(x-ys).get_mpz_t());
+      mpz_gcd(g.get_mpz_t(), lAbs.get_mpz_t(), N.get_mpz_t());
+
+      if(g > 1)
+        break;
+    }
+  }
+
+  return g;
 }
 
 
@@ -115,11 +126,24 @@ void BrentJob::InitThread()
 
 void BrentJob::Run()
 {
-    result = BrentFactorization( N );
 
-    // report that we are finished
-    if (callback_fn)
-        callback_fn( id );
+  while ( !isDying )
+  {
+    // if no one has found a result, run again
+    {
+      Lock lock ( mutex_result );
+      if ( result != N )
+        break;
+    }
+
+    mpz_class my_result = BrentFactorization( N );
+    if ( my_result != N )
+    {
+      Lock lock ( mutex_result );
+      result = my_result;
+    }
+  }
+
 }
 
 
@@ -129,17 +153,23 @@ void BrentJob::FlushThread()
 
 void ResultBrentFactorization ( unsigned id )
 {
-    {
-        Lock ( BrentManager.mutex_jobs );
+  {
+    Lock ( BrentManager.mutex_jobs );
 
-        if ( BrentManager.jobs[id]->result != BrentManager.N )
-        {
-            // call decryption function here
-            std::cout << BrentManager.jobs[id]->result << std::endl;
-            std::cout.flush();
-            return;
-        }
-    }
+    //mpz_class result = BrentManager.jobs[id]->result;
 
-    BrentManager.RunBrentFactorization(1);  // add a new job
+    delete BrentManager.jobs[id];
+    BrentManager.jobs.erase( BrentManager.jobs.begin() + id );
+
+    //if ( result != BrentManager.N )
+    //{
+    //  // call decryption function here
+    //  std::cout << result << std::endl;
+    //  std::cout.flush();
+    //  return;
+    //}
+  }
+
+
+  //BrentManager.RunBrentFactorization(1);  // add a new job
 }
